1498f1c7c7af15c8fe11c650003d91fde48357e0
John Lindquist <johnlindquist@gmail.com>
2019-03-09 18:02:13 -0500

    Sat Mar  9 18:02:13 EST 2019

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const shell = require("shelljs")
+const slugify = require("slugify")
+const fs = require("fs-extra")
+const { format } = require("date-fns")
+const formatTitle = require("ap-style-title-case")
+const cac = require("cac")
+const cli = cac()
+
+const blogPath = `/Users/johnlindquist/projects/johnlindquist.com`
+
+cli.command("[...title]").action(([text]) => {
+  const slug = slugify(text, {
+    replacement: "-", // replace spaces with replacement
+    remove: /[*+~.()'"!:@,]/g, // regex to remove characters
+    lower: true // result in lower case
+  })
+
+  const title = formatTitle(text)
+
+  const content = `---
+slug: ${slug}
+title: ${title}
+date: ${format(new Date(), "YYYY-MM-DD HH:MM")}
+published: false
+---
+
+# ${title}
+
+`
+
+  const filePath = `${blogPath}/content/blog/${slug}.md`
+
+  fs.writeFile(filePath, content)
+
+  shell.exec(`code ${blogPath}`)
+})
+
+cli.parse()
---
+++ yikes-this-will-be-fun.md
@@ -0,0 +1,6 @@
+---
+slug: yikes-this-will-be-fun
+title: Yikes, this will be fun
+date: Sat Mar 09 2019 11:24:50 GMT-0500 (Eastern Standard Time)
+published: true
+---  
