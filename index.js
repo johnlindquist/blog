854aaa20247a27a920ac13d821d688048820415f
John Lindquist <johnlindquist@gmail.com>
2019-09-27 11:48:07 -0600

    Fri Sep 27 11:48:07 MDT 2019

--- index.js
+++ index.js
@@ -7,40 +7,55 @@ const { format } = require("date-fns")
 const formatTitle = require("ap-style-title-case")
 const cac = require("cac")
 const cli = cac()
+const detectPort = require("detect-port")
 
 const blogPath = `/Users/johnlindquist/projects/johnlindquist.com`
 
-cli.command("[...title]").action(([text]) => {
-  const slug = slugify(text, {
-    replacement: "-", // replace spaces with replacement
-    remove: /[*+~.()'"!:@,]/g, // regex to remove characters
-    lower: true // result in lower case
-  })
+cli
+  .command("[...title]")
+  .option("--demo <demo>", "Codesandbox slug")
+  .action(([text], { demo }) => {
+    const slug = slugify(text, {
+      replacement: "-", // replace spaces with replacement
+      remove: /[*+~.()'"!:@,]/g, // regex to remove characters
+      lower: true // result in lower case
+    })
 
-  const title = formatTitle(text)
+    const title = formatTitle(text)
 
-  const content = `---
+    const demoBlock = demo && `<Codesandbox slug="${demo}"/>`
+
+    const content = `---
 slug: ${slug}
 title: ${title}
 date: ${format(new Date(), "YYYY-MM-DD HH:MM")}
 published: false
 ---
 
-# ${title}
-
+${demoBlock}
 `
 
-  const filePath = `${blogPath}/content/blog/${slug}.md`
+    const destination = demo ? "demo" : "blog"
 
-  fs.writeFile(filePath, content, { flag: "wx" }, function(err) {
-    if (err) {
-      console.log("file " + filePath + " already exists")
-    } else {
-      console.log("Succesfully written " + filePath)
-    }
+    shell.cd(blogPath)
+
+    const filePath = `${blogPath}/content/${destination}/${slug}.md`
+
+    ;(async () => {
+      try {
+        await fs.writeFile(filePath, content, { flag: "wx" })
+        console.log("Succesfully written " + filePath)
+
+        shell.exec(`code . -g ${filePath}`)
+        if ((await detectPort(8000)) === 8000) {
+          shell.exec(`open http://localhost:8000/${slug}`)
+          shell.exec(`yarn dev`)
+        }
+      } catch (error) {
+        console.log(error)
+        console.log("file " + filePath + " already exists")
+      }
+    })()
   })
-
-  shell.exec(`code ${blogPath} -g ${filePath}`)
-})
 
 cli.parse()
